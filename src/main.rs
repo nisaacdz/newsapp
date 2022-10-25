885c3174f0a2d9701b39a0b93539d845df84f5e0
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2022-10-25 12:01:11 +0000

    Implementing initial changes in functions and struct definitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ce262f1d2c1a4288a8960760763fc0b1";
+    let articles = get_articles(url);
+}
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>>{
+    let res = ureq::get(url).call()?.into_string()?;
+
+    dbg!(res);
+
+    todo!()
+}
+
+struct Article{
+    title: String,
+    url: String,
+}
+
+struct Articles{
+    articles: Vec<Article>,
 }
